7f8ba763fd2c39e2497b1667d653ab759bbdedf8
Sinikiwe Portia Xaba <XabaS2@ukzn.ac.za>
2022-11-13 20:49:42 +0200

    fronEnd

--- FrontEnd/src/app/api.service.ts
+++ FrontEnd/src/app/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  readonly apiUrl='http://localhost:4201/api/heroes';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getHeroes():Observable<any[]>{
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  addHero(val:any){
+    return this.http.post(this.apiUrl+'/Post',val);
+  }
 }
